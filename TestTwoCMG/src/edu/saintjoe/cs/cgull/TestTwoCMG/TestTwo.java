df0c3a83ff01f9e00b391c0222bfe5d377b262e0
cgull <cgull@localhost>
2016-04-06 15:09:31 -0400

    final commit

--- TestTwoCMG/src/edu/saintjoe/cs/cgull/TestTwoCMG/TestTwo.java
+++ TestTwoCMG/src/edu/saintjoe/cs/cgull/TestTwoCMG/TestTwo.java
@@ -1,10 +1,22 @@
 package edu.saintjoe.cs.cgull.TestTwoCMG;
 
 public class TestTwo {
+	//a code that is simple and has two methods, prints simply whether a number is even or odd 
 
 	public static void main(String[] args) {
-		// getting started
-
-	}
-
+		//make sure there is a main for this class
+		
+		int[] numbers = new int[]{1,2,3,4,5,6,7,8,9,10};
+		//create a list of possible numbers 
+		
+		for (int i = 0; i < numbers.length; i++) {
+			//create integer i, which is 0
+			//if the numbers are greater than i, then find which number is in accordance with the length
+		
+			if (numbers[i] =0)
+				System.out.println(numbers[i] + "odd");
+			else
+				System.out.println(numbers[i] + "even");}
+	} 			// print the correct word 
 }
+
---
+++ TestTwoCMG/src/edu/saintjoe/cs/cgull/TestTwoCMG/TestTwoDriver.java
@@ -0,0 +1,13 @@
+package edu.saintjoe.cs.cgull.TestTwoCMG;
+
+public class TestTwoDriver {
+
+	public static void main(String[] args) {
+		int.one = new int;
+		one.length = "1"; 
+		int.two = new int;
+		two.length = "2";
+		
+	}
+
+}
